06a29a1a6da7a5bb01e8dcb24a42f921c18ec011
Harshal2120 <harshal@qable.io>
2024-03-13 16:26:42 +0530

    added testng

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+
+public class MainTest {
+
+    @Test
+    public void testRun() {
+    }
+
+    @Test
+    public void testRun2() {
+    }
+
+    @Test
+    public void testRun3() {
+    }
+
+    @Test
+    public void testRun4() {
+    }
+
+    @Test
+    public void testRun5() {
+    }
+
+    @Test
+    public void testRun6() {
+    }
+
+    @Test
+    public void testRun7() {
+    }
+
+    @Test
+    public void testRun8() {
+    }
+
+    @Test
+    public void testRun9() {
+    }
+
+    @Test
+    public void testRun10() {
+    }
+
+    @Test
+    public void testRun11() {
+    }
+}
